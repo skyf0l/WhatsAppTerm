2963e9559387230b20ae4b76bcb02c275afe7b1c
Tom Rorato <skyf0l@localhost.localdomain>
2020-09-12 20:17:30 +0200

    Add report message

--- WhatsAppApi/utilities.py
+++ WhatsAppApi/utilities.py
@@ -17,14 +17,29 @@ def wait_until(somepredicate, timeout, period=0.05, *args, **kwargs):
 def eprint(msg):
     print(msg, file=sys.stderr)
 
-def eprint_report(msg, add_traceback=False):
-    RED_COLOR = '\033[91m'
+def print_report(msg, color=None, add_traceback=False):
+    START_COLOR = color if color is not None else ''
     END_COLOR = '\033[0m'
     report = msg + '\n'
     if add_traceback == True:
         report += traceback.format_exc()
-    report += 'Please, open an issue to fix it (hide private data)'
-    eprint(RED_COLOR + report + END_COLOR)
+    report += 'Please, to fix it, open an issue with this message (hide private data) and briefly explain how it happened'
+    eprint(START_COLOR + report + END_COLOR)
+
+def eprint_report(msg, add_traceback=False):
+    RED_COLOR = '\033[91m'
+    print_report(msg, color=RED_COLOR, add_traceback=add_traceback)
+
+def wprint_report(msg, add_traceback=False):
+    ORANGE_COLOR = '\033[93m'
+    print_report(msg, color=ORANGE_COLOR, add_traceback=add_traceback)
+
+def print_unknown_msg(message_tag, msg_data):
+    ORANGE_COLOR = '\033[93m'
+    END_COLOR = '\033[0m'
+    msg = '{},({}){}'.format(message_tag, str('json' if 'json' in msg_data else 'data'), str(msg_data['json'] if 'json' in msg_data else msg_data['data']))
+    report = 'Unknown msg: {}\nPlease, to fix it, open an issue with this message (hide private data) and briefly explain how it happened'.format(msg)
+    eprint(ORANGE_COLOR + report + END_COLOR)
 
 def get_whatsappweb_version():
     url = 'https://web.whatsapp.com/'
